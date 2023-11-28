0e3ba32eb60be3e43a79b1af9f10920abfc2b5e0
duanzin <lucaduan@gmail.com>
2023-11-28 17:11:09 -0300

    feat: skeleton of Pedidos page

--- src/routes/Pedidos.tsx
+++ src/routes/Pedidos.tsx
@@ -4,7 +4,26 @@ function Pedidos() {
   return (
     <>
       <Header />
-      <main>pedidos</main>
+      <main>
+        <section>
+          <h2>Categorias</h2>
+          <span>Navegue por categoria</span>
+          <ul className="flex flex-row">
+            <li>Combos</li>
+            <li>acompanhamentos</li>
+            <li>Bebidas</li>
+            <li>Sobremesas</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Produtos</h2>
+          <span>Selecione um produto para adicionar ao seu pedido</span>
+        </section>
+        <footer className="flex flex-row">
+          <button>Cancelar</button>
+          <button>Finalizar pedido</button>
+        </footer>
+      </main>
     </>
   );
 }
