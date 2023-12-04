31d8ac71f1f8941e18b5e62fc15555c23f86c73d
duanzin <lucaduan@gmail.com>
2023-12-04 16:43:38 -0300

    style: altered width of orderRender li

--- src/routes/Cozinha.tsx
+++ src/routes/Cozinha.tsx
@@ -23,14 +23,14 @@ function Cozinha() {
     <>
       <Header />
       <main className="flex flex-row w-5/6 gap-y-5 m-auto mb-20 max-[1000px]:w-11/12 max-[900px]:flex-col max-[900px]:items-center">
-        <section className="w-1/2 border-r-2 border-black border-solid max-[900px]:border-0 max-[900px]:w-fit">
+        <section className="w-1/2 border-r-2 border-black border-solid max-[900px]:border-0 max-[900px]:w-full">
           <h2 className="text-4xl font-bold">Preparando:</h2>
           <OrderRender
             orders={orders.filter((order) => order.status === 0)}
             setOrders={setOrders}
           />
         </section>
-        <section className="w-1/2 pl-10 max-[900px]:p-0 max-[900px]:w-fit">
+        <section className="w-1/2 pl-10 max-[900px]:p-0 max-[900px]:w-full">
           <h2 className="text-4xl font-bold">Pronto:</h2>
           <OrderRender
             orders={orders.filter((order) => order.status === 1)}
