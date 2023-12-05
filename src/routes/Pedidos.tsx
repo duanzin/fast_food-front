2c9c549b353868070d7888e86b4e517fb27c1879
duanzin <lucaduan@gmail.com>
2023-12-05 17:13:56 -0300

    feat: moved form to send orders to another component

--- src/routes/Pedidos.tsx
+++ src/routes/Pedidos.tsx
@@ -4,9 +4,9 @@ import Categorias from "../components/Categorias";
 import Produtos from "../components/Produtos";
 import SearchBar from "../components/SearchBar";
 import Resumo from "../components/Resumo";
-import { OrderDetails, SelectedProduct } from "../utils/types";
+import { SelectedProduct } from "../utils/types";
 import { FoodOptionItem } from "../utils/types";
-import { create } from "../api/apiCalls";
+import OrderForm from "../components/OrderForm";
 
 function Pedidos() {
   const [foundItemId, setFoundItemId] = useState<string | null>(null);
@@ -15,8 +15,6 @@ function Pedidos() {
   );
   const [totalAmount, setTotalAmount] = useState<number>(0);
   const [finishPurchase, setFinishPurchase] = useState<boolean>(false);
-  const [customer, setCustomer] = useState<string>("");
-  const [observation, setObservation] = useState<string>("");
 
   const handleProductSelect = (product: FoodOptionItem, quantity: number) => {
     const existingProductIndex = selectedProducts.findIndex(
@@ -40,109 +38,16 @@ function Pedidos() {
     setFinishPurchase(false);
   };
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCustomer(e.target.value);
-  };
-
-  const handleObservationChange = (
-    e: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setObservation(e.target.value);
-  };
-
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const trimmedCustomer: string = customer.trim();
-    const trimmedObservation: string = observation.trim();
-    if (!trimmedCustomer) {
-      alert("por favor escreva seu nome");
-    } else {
-      try {
-        const orderDetails: OrderDetails = {
-          customer: customer,
-          products: selectedProducts.map((product) => ({
-            name: product.name,
-            quantity: product.quantity,
-          })),
-        };
-        if (
-          trimmedObservation !== null &&
-          trimmedObservation !== undefined &&
-          trimmedObservation !== ""
-        ) {
-          orderDetails.observation = trimmedObservation;
-        }
-        await create(orderDetails);
-        setFinishPurchase(false);
-        setSelectedProducts([]);
-        setTotalAmount(0);
-        setCustomer("");
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  };
-
   return (
     <>
       <Header />
       <main className="w-5/6 m-auto mb-20 max-[1200px]:w-11/12">
         {finishPurchase ? (
-          <form onSubmit={handleFormSubmit}>
-            <h2 className="text-4xl font-bold">Pagamento</h2>
-            <article>
-              <strong className="text-lg">Resumo da compra</strong>
-              <Resumo
-                selectedProducts={selectedProducts}
-                totalAmount={totalAmount}
-              />
-            </article>
-            <article className="flex flex-col mb-20">
-              <label htmlFor="customer" className="font-bold text-lg">
-                Nome do cliente
-              </label>
-              <input
-                type="text"
-                id="customer"
-                name="customer"
-                placeholder="Primeiro nome"
-                maxLength={20}
-                autoComplete="off"
-                value={customer}
-                onChange={handleNameChange}
-                className="bg-gray-100 p-4 w-96 rounded-md placeholder:text-lg 
-                max-[415px]:w-full focus:outline-none"
-              />
-              <label htmlFor="obs" className="font-bold text-lg">
-                Observações
-              </label>
-              <textarea
-                id="obs"
-                name="obs"
-                placeholder="Adicione uma observação ao pedido"
-                value={observation}
-                onChange={handleObservationChange}
-                className="bg-gray-100 p-4 h-52 w-full rounded-md placeholder:text-lg 
-                resize-none focus:outline-none"
-              />
-            </article>
-            <footer className="flex flex-row justify-end items-center gap-x-16 gap-y-5 max-[510px]:flex-col">
-              <button
-                onClick={handleCancel}
-                className="text-2xl text-green-800 font-bold w-80 h-16 
-          border-4 border-green-800 rounded-3xl max-[370px]:w-11/12"
-              >
-                Cancelar
-              </button>
-              <button
-                type="submit"
-                className="text-2xl text-white font-bold w-80 h-16 rounded-3xl
-           bg-green-800 max-[370px]:w-11/12"
-              >
-                Finalizar pedido
-              </button>
-            </footer>
-          </form>
+          <OrderForm
+            selectedProducts={selectedProducts}
+            totalAmount={totalAmount}
+            handleCancel={handleCancel}
+          />
         ) : (
           <>
             <SearchBar setFoundItemId={setFoundItemId} />
