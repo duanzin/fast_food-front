93e48f3f2068596188129b657fb4a58c3fc5847e
duanzin <lucaduan@gmail.com>
2023-12-05 23:17:25 -0300

    chore: modified env variable name

--- src/api/apiCalls.tsx
+++ src/api/apiCalls.tsx
@@ -2,21 +2,20 @@ import axios from "axios";
 import { OrderDetails } from "../utils/types";
 
 export async function getMeals() {
-  console.log(process.env);
-  const res = await axios.get(`${process.env.REACT_APP_API_URL}/`);
+  const res = await axios.get(`${process.env.VITE_API_URL}/`);
   return res.data;
 }
 
 export async function create(body: OrderDetails) {
-  await axios.post(`${process.env.REACT_APP_API_URL}/`, body);
+  await axios.post(`${process.env.VITE_API_URL}/`, body);
 }
 
 export async function update(id: number) {
-  const res = await axios.patch(`${process.env.REACT_APP_API_URL}/${id}`);
+  const res = await axios.patch(`${process.env.VITE_API_URL}/${id}`);
   return res.data;
 }
 
 export async function remove(id: number) {
-  const res = await axios.delete(`${process.env.REACT_APP_API_URL}/${id}`);
+  const res = await axios.delete(`${process.env.VITE_API_URL}/${id}`);
   return res.data;
 }
