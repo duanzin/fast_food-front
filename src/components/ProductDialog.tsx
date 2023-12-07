cb87919e6fbf953b252578a0a3c76d2e9387ba1f
duanzin <lucaduan@gmail.com>
2023-12-07 17:02:01 -0300

    style: general style of dialog

--- src/components/ProductDialog.tsx
+++ src/components/ProductDialog.tsx
@@ -13,21 +13,52 @@ function ProductDialog({
   addProduct: (product: FoodOptionItem, quantity: number) => void;
 }) {
   const [quantity, setQuantity] = useState<number>(1);
+  const handleAddToOrder = () => {
+    addProduct(product, quantity);
+    onClose();
+  };
+
   return (
-    <dialog open={isOpen} onClose={onClose}>
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
-      <span>Price: R${product.price.toFixed(2).replace(".", ",")}</span>
-      <div>
-        <button onClick={() => setQuantity(quantity - 1)}>-</button>
-        <span>Quantity: {quantity}</span>
-        <button onClick={() => setQuantity(quantity + 1)}>+</button>
+    <dialog
+      open={isOpen}
+      onClose={onClose}
+      className="fixed top-1/3 p-10 bg-white rounded-xl"
+    >
+      <header className="flex flex-row justify-between">
+        <h2 className="text-4xl font-bold">Revise seu pedido!</h2>
+        <button onClick={onClose} className="text-3xl text-gray-500 font-bold">
+          X
+        </button>
+      </header>
+      <div className="flex flex-row">
+        <img src={product.image} alt={product.name} className="w-36 h-36" />
+        <div>
+          <strong className="text-2xl">{product.name}</strong>
+          <p className="text-xl">{product.description}</p>
+          <div className="w-fit text-2xl border-solid border-y-2 border-green-800 rounded-full">
+            <button
+              onClick={() => setQuantity(quantity - 1)}
+              className="text-white text-center font-bold bg-green-800 rounded-full w-10 h-10"
+            >
+              -
+            </button>
+            <span>{quantity}</span>
+            <button
+              onClick={() => setQuantity(quantity + 1)}
+              className="text-white text-center font-bold bg-green-800 rounded-full w-10 h-10"
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <strong className="text-2xl">
+          R${product.price.toFixed(2).replace(".", ",")}
+        </strong>
       </div>
-      <button onClick={onClose}>Cancelar</button>
       <button
-        onClick={() => {
-          addProduct(product, quantity);
-        }}
+        onClick={handleAddToOrder}
+        className="text-2xl text-white font-bold w-80 h-16 rounded-3xl
+           bg-green-800"
       >
         Adicionar ao pedido
       </button>
